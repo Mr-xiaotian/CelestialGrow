464214982a439a1275a8d71fec008c03900c11c0
Mr-xiaotian <273420001@qq.com>
2026-08-26 04:22:42 +0800

    feat(api): add `api/`

---
+++ pkg/api/api.go
@@ -0,0 +1,41 @@
+package api
+
+import (
+	"time"
+
+	"github.com/Mr-xiaotian/CelestialGrow/pkg/farm"
+	"github.com/Mr-xiaotian/CelestialGrow/pkg/plot"
+)
+
+// Farm 是对外暴露的农场类型。
+type Farm = farm.Farm
+
+// Plot 是对外暴露的泛型节点类型。
+type Plot[S any, F any] = plot.Plot[S, F]
+
+// PlotNode 是 Farm 连接 plot 时使用的统一接口。
+type PlotNode = plot.PlotNode
+
+// Option 是 Plot 的可选配置。
+type Option = plot.Option
+
+// NewFarm 创建一个 Farm。
+func NewFarm(name string, logLevel string) *Farm {
+	return farm.NewFarm(name, logLevel)
+}
+
+// NewPlot 创建一个 Plot。
+func NewPlot[S any, F any](name string, cultivator func(S) (F, error), opts ...Option) *Plot[S, F] {
+	return plot.NewPlot(name, cultivator, opts...)
+}
+
+var (
+	WithTends      = plot.WithTends
+	WithChanSize   = plot.WithChanSize
+	WithMaxRetries = plot.WithMaxRetries
+	WithRetryDelay = func(fn func(int) time.Duration) Option {
+		return plot.WithRetryDelay(fn)
+	}
+	WithRetryIf  = plot.WithRetryIf
+	WithLogLevel = plot.WithLogLevel
+)

8cc3c605833ebd55e54d88fd3149537be1309d88
Mr-xiaotian <273420001@qq.com>
2026-08-30 14:18:11 +0800

    chore(demo/gitignore): add `demo/`, and add path in `.gitignore`

---
+++ demo/demo_farm.go
@@ -0,0 +1,32 @@
+package main
+
+import grow "github.com/Mr-xiaotian/CelestialGrow/pkg/api"
+
+// double 将种子翻倍。
+func double(num int) (int, error) {
+	return num * 2, nil
+}
+
+// addOne 为种子加一。
+func addOne(num int) (int, error) {
+	return num + 1, nil
+}
+
+// main 演示一条 Farm 流水线：root 将种子翻倍后传给 head 加一。
+func main() {
+	root := grow.NewPlot("root", double, grow.WithTends(2))
+	head := grow.NewPlot("head", addOne, grow.WithTends(2))
+
+	farm := grow.NewFarm("demo_farm", "INFO")
+	if err := farm.AddPlot(root, head); err != nil {
+		panic(err)
+	}
+	if err := farm.Connect([]grow.PlotNode{root}, []grow.PlotNode{head}); err != nil {
+		panic(err)
+	}
+	if err := farm.Run(map[string][]any{
+		"root": {1, 2, 3},
+	}); err != nil {
+		panic(err)
+	}
+}
